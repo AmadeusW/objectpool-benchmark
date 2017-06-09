12123d1a7347b13bf1f145885d68a10d83eb60de
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-08 23:08:18 -0700

    Creates a basic benchmark program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+using System;
 
-namespace objectPools
+namespace A6z.ObjectPools
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = ManualConfig.Create(DefaultConfig.Instance);
+            config.Set(new BenchmarkDotNet.Reports.SummaryStyle()
+            {
+                PrintUnitsInHeader = true,
+                PrintUnitsInContent = false,
+                TimeUnit = BenchmarkDotNet.Horology.TimeUnit.Millisecond,
+                SizeUnit = BenchmarkDotNet.Columns.SizeUnit.KB
+            });
+            config.Add(new MemoryDiagnoser());
+            var summary = BenchmarkRunner.Run<MemoryTests>(config);
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class MemoryTests
+    {
+        [Benchmark(Description = "Allocate 100B")]
+        public byte[] Allocate()
+        {
+            return new byte[100];
         }
     }
 }
