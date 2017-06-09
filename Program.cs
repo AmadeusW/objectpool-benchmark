a28e26650b38a56bd0cd588add69d1888a1b0bf1
Amadeusz Wieczorek <git@amadeusw.com>
2017-06-08 21:48:23 -0700

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace objectPools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
